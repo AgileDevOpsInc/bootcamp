ddda5f80e4093ab6198a808662d342c7e5aaa3d6
GitHub Developer <deraviyam@gmail.com>
2020-07-27 13:10:30 +0530

    Update installnodejs.sh

--- installnodejs.sh
+++ installnodejs.sh
@@ -1,28 +1,5 @@
-#!/bin/bash 
-
-export NODE_VERSION=4.8.6
-
-apk add --no-cache libstdc++ 
-apk add --no-cache --virtual .build-deps \
-        binutils-gold \
-        curl \
-        g++ \
-        gcc \
-        gnupg \
-        libgcc \
-        linux-headers \
-        make \
-        python 
-
-curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz" 
-tar -xf "node-v$NODE_VERSION.tar.xz" 
-cd "node-v$NODE_VERSION" 
-./configure 
-make -j$(getconf _NPROCESSORS_ONLN) 
-make install 
-apk del .build-deps 
-cd .. \
-rm -rf node-v$NODE_VERSION.tar.xz node-v$NODE_VERSION
-
-
-exit 0
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+nodejs -v
+sudo npm install npm -g
